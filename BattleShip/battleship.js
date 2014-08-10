c0602ef5feaf42e7cbc643954a00d50be493cf37
Mari Wahl <mari.wahl9@gmail.com>
2014-08-09 21:56:59 -0400

    battleship oop  win

--- BattleShip/battleship.js
+++ BattleShip/battleship.js
@@ -1,196 +1,211 @@
-// **************************************************************//
-//              Marina Wahl - dev.mariwahl.us  - 2014            //
-//                   BATTLESHIP    IN JAVASCRIPT                 //
-//                                                               //
-//                                                               //
-// **************************************************************//
+/* Attributes and methods for the shipModel function */
 
+var shipModel = function() {
+    this.boardSize = 7;
+    this.numShips = 3;
+    this.shipLength = 3;
+    this.shipsSunk = 0;
+    this.ships = [{
+        locations: [0, 0, 0],
+        hits: ["", "", ""]
+    }, {
+        locations: [0, 0, 0],
+        hits: ["", "", ""]
+    }, {
+        locations: [0, 0, 0],
+        hits: ["", "", ""]
+    }];
+};
 
-
-var Battleship = {
-
-    BOARD_WIDTH  : 20,
-    BOARD_HEIGTH :  20,
-    NUMBER_SHIPS_3 :  2,
-    NUMBER_SHIPS_2 : 3,
-    SIZE_CELL : 1,
-    BACKGROUND_COLOR : "#000000",
-    CELL_COLOR : "#66FF66",
-    cells_guesses : [],
-    cells_ships : [],
-    gameover : false,
-    ships_left : this.NUMBER_SHIPS_3*3 + this.NUMBER_SHIPS_2*2,
-    guesses_left : this.BOARD_WIDTH + this.BOARD_HEIGTH,
-
-
-    clearBoard : function() {
-        // Fills the board with 0S
-        // (just clean the board for a new game)
-        for (var i = 0; i < this.BOARD_WIDTH; i++) {
-            this.cells_guesses[i] = [];
-            this.cells_ships [i]= [];
-            for (var j = 0; j < this.BOARD_HEIGTH; j++) {
-                this.cells_guesses[i][j] = 0;
-                this.cells_ships[i][j]   = 0;
-            }
-        }
-    },
-
-
-
-    generateShips : function(){
-        // create (pseudo)-random ships
-        // TODO: need to make the landscape works or
-        // find a better way to be randm
-        // TODO: is there a smarter way of doing this without
-        // using so many ifs??? maybe case/switch?
-        this.clearBoard();
-        var k = this.NUMBER_SHIPS_3;
-        landscape = true;
-        while (k > 0){
-            var ship_x = Math.floor(Math.random() * this.BOARD_WIDTH-3)
-            var ship_y = Math.floor(Math.random() * this.BOARD_HEIGTH-3)
-            if (this.cells_ships[ship_x][ship_y] == 0){
-                if (landscape == true && this.cells_ships[ship_x+1][ship_y] == 0 && this.cells_ships[ship_x+2][ship_y] == 0){
-                    this.cells_ships[ship_x][ship_y] = 3;
-                    this.cells_ships[ship_x+1][ship_y] = 3;
-                    this.cells_ships[ship_x+2][ship_y] = 3;
-                    k--;
-                    document.write(this.cells_ships[ship_x][ship_y]);
-                    //landscape = not landscape;
-                } else if (landscape != false && this.cells_ships[ship_x][ship_y+1] == 0&& this.cells_ships[ship_x][ship_y+2] == 0){
-                    this.cells_ships[ship_x][ship_y] = 3;
-                    this.cells_ships[ship_x][ship_y+1] = 3;
-                    this.cells_ships[ship_x][ship_y+2] = 3;
-                    k--;
-                    //landscape = not landscape;
-                }
-            }
-        }
-
-        var k = this.NUMBER_SHIPS_2;
-        while (k > 0){
-            ship_x = Math.floor(Math.random() * this.BOARD_WIDTH-2)
-            ship_y = Math.floor(Math.random() * this.BOARD_HEIGTH-2)
-            if (this.cells_ships[ship_x][ship_y] == 0){
-                 if (landscape == true && this.cells_ships[ship_x+1][ship_y] == 0){
-                    this.cells_ships[ship_x][ship_y] = 2;
-                    this.cells_ships[ship_x+1][ship_y] = 2;
-                    k--;
-                    //landscape = not landscape;
-                } else if (landscape != true && this.cells_ships[ship_x][ship_y+1] == 0){
-                    this.cells_ships[ship_x][ship_y] = 2;
-                    this.cells_ships[ship_x][ship_y+1] = 2;
-                    k--;
-                    //landscape =  landscape;
-                }
-            }
-        }
-    },
-
-
-
-    printBoardAscII: function(){
-        // print the board in ascii -> for debug only
-        for (var i = 0; i < this.BOARD_WIDTH; i++) {
-            for (var j = 0; j < this.BOARD_HEIGTH; j++) {
-                document.write(this.cells_ships[i][j]);
-            }
-            document.write("<br>");
-        }
-        document.write("<br><br>")
-    },
-
-
-    printBoardPixel : function(i, j) {
-        // final game printing
-        // todo: this function needs work,
-        // write the coordinate numbers
-        if (this.cells_ships[i][j] != 0) {
-            shift_i = i * this.SIZE_CELL;
-            shift_j = j * this.SIZE_CELL;
-            context.beginPath();
-            context.fillStyle = CELL_COLOR;
-            context.fillRect(shift_i, shift_j, SIZE_CELL, SIZE_CELL);
-            context.closePath();
-            context.fill();
+shipModel.prototype.fire = function(guess) {
+    for (var i = 0; i < this.numShips; i++) {
+        var ship = this.ships[i];
+        var index = ship.locations.indexOf(guess);
+        if (ship.hits[index] === "hit") {
+            view.displayMessage(ALREADY_HIT);
             return true;
-        } else {
+        } else if (index >= 0) {
+            ship.hits[index] = "hit";
+            view.displayHit(guess);
+            view.displayMessage(HIT);
+
+            if (this.isSunk(ship)) {
+                view.displayMessage(YOU_SANK_ONE);
+                this.shipsSunk++;
+            }
+            return true;
+        }
+    }
+    view.displayMiss(guess);
+    view.displayMessage(YOU_MISSED);
+    return false;
+};
+
+shipModel.prototype.isSunk = function(ship) {
+    for (var i = 0; i < this.shipLength; i++) {
+        if (ship.hits[i] !== "hit") {
             return false;
         }
-    },
+    }
+    return true;
+};
+
+shipModel.prototype.generateShipLocations = function() {
+    var locations;
+    for (var i = 0; i < this.numShips; i++) {
+        do {
+            locations = this.generateShip();
+        } while (this.collision(locations));
+        this.ships[i].locations = locations;
+    }
+    console.log("Ships array: ");
+    console.log(this.ships);
+};
 
 
-
-    gameOverMsg: function(msg){
-        // Finishes the game
-        // TODO: make better messages and
-        // ask if want to play again
-        if (msg == "winner"){
-            alert("Congrats!!!");
-        }else if (msg == "loser"){
-            alert("I'm sorry, you lost!");
+shipModel.prototype.generateShip = function() {
+    var direction = Math.floor(Math.random() * 2);
+    var row, col;
+    if (direction === 1) { // horizontal
+        row = Math.floor(Math.random() * this.boardSize);
+        col = Math.floor(Math.random() * (this.boardSize - this.shipLength + 1));
+    } else { // vertical
+        row = Math.floor(Math.random() * (this.boardSize - this.shipLength + 1));
+        col = Math.floor(Math.random() * this.boardSize);
+    }
+    var newShipLocations = [];
+    for (var i = 0; i < this.shipLength; i++) {
+        if (direction === 1) {
+            newShipLocations.push(row + "" + (col + i));
+        } else {
+            newShipLocations.push((row + i) + "" + col);
         }
-    },
+    }
+    return newShipLocations;
+};
 
-
-    checkGuess: function(guess_x, guess_y){
-        // check if the guess is good
-        // TODO: if finishes the ship, should be able
-        // to say "sink sunk or something"
-        if (this.cells_ships[guess_x][guess_y] > 0){
-            this.ships_left --;
+shipModel.prototype.collision = function(locations) {
+    for (var i = 0; i < this.numShips; i++) {
+        var ship = this.ships[i];
+        for (var j = 0; j < locations.length; j++) {
+            if (ship.locations.indexOf(locations[j]) >= 0) {
+                return true;
+            }
         }
-    },
-
-    getGuess : function(){
-        // get user's guest
-        // TODO: any other better way than the annoying
-        // prompt????
-        var guess_x = prompt("Guess a x coordinate:");
-        var guess_y = prompt("Now, guess a y coordinate:");
-        this.guesses_left --;
-        console.log(this.guesses_left);
-        this.checkGuess(guess_x, guess_y);
-    },
-
-
-    startGame : function() {
-        // the game runs here
-        // TODO: is this loop ok???
-        // TODO: is the gameover checking ok???
-        this.generateShips();
-        this.printBoardAscII();
-        //this.printBoardPixel();
-        while (this.gameover != true){
-            this.getGuess();
-            if (this.ships_left == 0){
-              this.gameOverMsg("winner");
-              this.gameover = true;
-            } else if (this.guesses_left == 0){
-                this.gameOverMsg("loser");
-                this.gameover = true;
-           }
-        }
-     }
-
-
+    }
+    return false;
 };
 
 
 
+/* process message */
+var view = {
+    displayMessage: function(msg) {
+        var messageArea = document.getElementById("messageArea");
+        messageArea.innerHTML = msg;
+    },
+    displayHit: function(location) {
+        var cell = document.getElementById(location);
+        cell.setAttribute("class", "hit");
+    },
+    displayMiss: function(location) {
+        var cell = document.getElementById(location);
+        cell.setAttribute("class", "miss");
+    }
 
-// ***************************************************************//
-//                                                                //
-//                THE GAME STARTS HERE                            //
-//                                                                //
-// ***************************************************************//
+};
 
 
-// create html canvas
-//var game_canvas = document.getElementById("game");
-//if (game_canvas.getContext) {
-//    var context = game_canvas.getContext('2d');
-//}
+/* Process guesses values */
+var controller = {
+    guesses: 0,
+    processGuess: function(guess) {
+        var location = parseGuess(guess);
+        if (location) {
+            this.guesses++;
+            var hit = shipModel.fire(location);
+            if (hit && shipModel.shipsSunk === shipModel.numShips) {
+                view.displayMessage(YOU_SANK + this.guesses);
+            }
+        }
+    }
+};
 
-Battleship.startGame();
+
+/* Helper function to parse a guess from the user */
+function parseGuess(guess) {
+    var alphabet = ["A", "B", "C", "D", "E", "F", "G"];
+    if (guess === null || guess.length !== 2) {
+        alert(GUESS_INSTRUCTIONS);
+    } else {
+        var firstChar = guess.charAt(0);
+        var row = alphabet.indexOf(firstChar);
+        var column = guess.charAt(1);
+
+        if (isNaN(row) || isNaN(column)) {
+            alert(NOT_VALID);
+        } else if (row < 0 || row >= shipModel.boardSize ||
+            column < 0 || column >= shipModel.boardSize) {
+            alert(NOT_BOARD);
+        } else {
+            return row + column;
+        }
+    }
+    return null;
+};
+
+
+
+/* event handlers */
+function handleFireButton() {
+    var guessInput = document.getElementById("guessInput");
+    var guess = guessInput.value.toUpperCase();
+    controller.processGuess(guess);
+    guessInput.value = "";
+};
+
+
+
+/* Here we just handle the fire button/ inputs */
+function handleKeyPress(e) {
+    var fireButton = document.getElementById("fireButton");
+    if (e.keyCode === 13) {
+        fireButton.click();
+        return false;
+    }
+};
+
+
+
+/* The game starts here.
+    init: called when the page has completed loading
+    */
+// Text variables
+GUESS_INSTRUCTIONS = "Please enter a letter and a number within the board."
+NOT_BOARD = "Please choose something within the board."
+NOT_VALID = "Please choose valid values."
+YOU_SANK_ONE = "Oh, no!!! You sank my battleship!!!"
+YOU_SANK = "You sank all my battleships. Number of Guesses: "
+ALREADY_HIT = "You already hit that location!!!"
+HIT = "You hit one of my battleship!!!"
+YOU_MISSED = "You missed..."
+
+// Instance of a new ship
+var shipModel = new shipModel();
+
+// when the windows is loaded
+window.onload = init;
+
+// the traditional init function
+function init() {
+    // Fire! button onclick handler
+    var fireButton = document.getElementById("fireButton");
+    fireButton.onclick = handleFireButton;
+
+    // handle "return" key press
+    var guessInput = document.getElementById("guessInput");
+    guessInput.onkeypress = handleKeyPress;
+
+    // place the ships on the game board
+    shipModel.generateShipLocations();
+}
